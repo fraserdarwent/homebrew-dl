fe5b97f1af2d27a74a704aabb71e8c3defc11e7a
GitHub Action <action@github.com>
2020-11-15 21:53:10 +0000

    Update Formula

--- Formula/dl.rb
+++ Formula/dl.rb
@@ -2,7 +2,7 @@ lass Dl < Formula
   desc "A small download utility"
   homepage "https://github.com/fraserdarwent/dl"
   url "https://github.com/fraserdarwent/dl/releases/download/0.0.6/dl-0.0.6-darwin-amd64.zip"
-  sha256 "3d1c576a35c00b600649d1b686d2e8a6fc22cc7a18f61f89fe88e982522e8377  dl-0.0.6-darwin-amd64.zip"
+  sha256 "3d1c576a35c00b600649d1b686d2e8a6fc22cc7a18f61f89fe88e982522e8377"
   version "0.0.6"
   license "MIT"
 

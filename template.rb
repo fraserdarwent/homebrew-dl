972b06a7eed018eb59b40607a5cc5a09f924a338
Fraser Darwent <fraser@darwen.tf>
2020-11-15 21:45:11 +0000

    Update

---
+++ template.rb
@@ -0,0 +1,12 @@
+lass Dl < Formula
+  desc "A small download utility"
+  homepage "https://github.com/fraserdarwent/dl"
+  url "${URL}"
+  sha256 "${SHA}"
+  version "${VERSION}"
+  license "MIT"
+
+  def install
+    bin.install "dl"
+  end
+end
